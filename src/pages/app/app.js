9a803f015df894057f351fed3d66d70f46e800e1
Ibrahim Ibrahim <folahanmi0001@gmail.com>
2022-02-12 04:37:42 +0100

    minor changes - side navigation

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -3,52 +3,32 @@ import "./app.scss";
 function App() {
   return (
     <div className="home mx-auto">
-      {/* <div className="home__wrapper flex flex-row flex-wrap py-4 columns-5">
-        <nav className="sideNav w-full sm:w-1/3 md:w-1/4 px-2">Column 1</nav>
-        <main>Column 2</main>
-        <div>Column 3</div>
-      </div> */}
-      {/* <div className="home__wrapper grid grid-flow-col">
-        <aside className="sidenav bg-gray-600 w-1/3">
-          <nav class="">Nav</nav>
-        </aside>
-        <main role="main" class="main bg-lime-300 w-1/3">
-          Main
-        </main>
-        <aside class="right bg-orange-400">Right</aside>
-      </div> */}
       <div className="home__wrapper flex flex-row flex-wrap">
-        <aside className="sidenav bg-zinc-900 h-screen text-white flex py-5 px-7">
-          <nav class="sidenav__nav sticky flex flex-col justify-between">
-            <div className="sidenav__section">Play Now</div>
-            {/* section b */}
-            <div className="sidenav__navItems">
-              <div className="sidenav__navItem">
-                {/* icon */}
-                Home
-              </div>
-              <div className="sidenav__navItem">
-                {/* icon */}
-                Search
-              </div>
-              <div className="sidenav__navItem">
-                {/* icon */}
-                Your Library
-              </div>
-              <div className="sidenav__navItem mt-5">
-                {/* icon */}
-                Create Playlist
-              </div>
-              <div className="sidenav__navItem">
-                {/* icon */}
-                Liked Songs
-              </div>
-              <hr className="mt-4 mb-3 w-40" />
-              <div className="sidenav__navItem">Instrumental</div>
-              <div className="sidenav__navItem">All Out 90s</div>
-              <div className="sidenav__navItem">Blues Rock</div>
+        <aside className="sidenav bg-zinc-900 h-screen text-white flex py-6 px-7">
+          <nav class="sidenav__nav sticky">
+            <div className="sidenav__playTitle flex flex-row justify-between">
+              <div className="sidenav__playTitleBtn px-5">Just Play</div>
+              <div></div>
             </div>
-            <div>Install App</div>
+            {/* section b */}
+            <div className="sidenav__navItems mt-6">
+              <div className="sidenav__navItem mt-4">Home</div>
+              <div className="sidenav__navItem mt-4">Compilation</div>
+              <div className="sidenav__navItem mt-4">Notifications</div>
+              <div className="sidenav__navItem mt-4">Chat</div>
+              <div className="sidenav__navItem mt-4">Search</div>
+              <div className="sidenav__navItem mt-4">Library</div>
+              <div className="sidenav__navItem mt-6">Liked Songs</div>
+            </div>
+            <div className="divider my-5 w-32" />
+            <div className="sidenav__playists">
+              <div className="sidenav__playlistsTitle">
+                <div>PLAYLISTS</div>
+              </div>
+              <div className="sidenav__playlistsItem mt-4">Kid Cudi Zone</div>
+              <div className="sidenav__playlistsItem mt-3">British Rock</div>
+              <div className="sidenav__playlistsItem mt-3">Disco Elysium</div>
+            </div>
           </nav>
         </aside>
         <main role="main" class="main bg-lime-300">
